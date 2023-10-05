4b3901c02aae9a385a680dcf1cf830eef19eb726
Denilson_mart <denilson.martins1236@gmail.com>
2023-10-05 12:38:30 -0300

    calculator - final changes in style and script

--- calculator-dark-light/script.js
+++ calculator-dark-light/script.js
@@ -1,24 +1,25 @@
+//evento usado para resolver o problema em que o querrySelector não estava funcionando, esse evento faz que o código seja rodado somente após ser carregado todos os elementos do DOM
 document.addEventListener("DOMContentLoaded", function () {
+  //obtendo todos os elementos necessários para o código
   const toggleBtn = document.querySelector(".toggleBtn");
   const body = document.querySelector("body");
   let btn = document.querySelectorAll("span");
   let value = document.querySelector("#value");
 
 
-  for (let i = 0; i < btn.length; i++) {
-        btn[i].addEventListener("click", function () {
-          if(this.innerHTML == "="){
-            value.innerHTML = eval(value.innerHTML);
-          }
-          else {
-            if(this.innerHTML == "Clear"){
-              value.innerHTML =  "";
-            } else {
-              value.innerHTML += this.innerHTML;
-            }
-          }
-        });
+  btn.forEach(function(tecla){
+    tecla.addEventListener("click", function () {
+      if (tecla.innerHTML == "=") {
+        value.innerHTML = eval(value.innerHTML)
+      } else {
+        if (tecla.innerHTML == "Clear") {
+          value.innerHTML = ""
+        } else {
+          value.innerHTML += tecla.innerHTML
+        }
       }
+    })
+  }) 
       //evento para a mudança de modo aplicado para quando o toggleBtn for clicado
       toggleBtn.onclick = function () {
         body.classList.toggle("dark");
